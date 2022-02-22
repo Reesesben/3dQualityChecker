fd9182933bbeca9138f08b986196a4e220226a76
Ben Erekson <42879076+Reeseben@users.noreply.github.com>
2022-02-22 04:31:59 -0700

    Adjust gitnore file, Add XCode swift ui project

---
+++ QualityChecker/QualityChecker/QualityCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  QualityCheckerApp.swift
+//  QualityChecker
+//
+//  Created by Ben Erekson on 2/22/22.
+//
+
+import SwiftUI
+
+@main
+struct QualityCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
